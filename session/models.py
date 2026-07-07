8c19c633acb33df6600b6638391d5b9befcd97f9
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-07-07 22:55:39 +0330

    Fix model session

--- session/models.py
+++ session/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Session(models.Model):
     session_key = models.CharField(max_length=64, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    expires_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.session_key}"
