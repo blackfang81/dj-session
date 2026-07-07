4267ef649dfe3af01e36db7263dab6238bd0bac9
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-07-07 22:51:11 +0330

    Add model session

--- session/models.py
+++ session/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Session(models.Model):
+    session_key = models.CharField(max_length=64, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    expires_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
